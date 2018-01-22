0f4dab8485f3c154fda863023a2941f8dfed0454
Jaime Tena <jaime.tena@onyxcentersource.com>
2018-01-22 17:41:46 +0100

    Guardar un objeto serializado

---
+++ src/main/java/Box.java
@@ -0,0 +1,33 @@
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+public class Box implements Serializable {
+
+    private int width;
+    private int height;
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public static void main(String[] args) {
+        Box myBox = new Box();
+
+        myBox.setHeight(50);
+        myBox.setWidth(20);
+
+        try {
+            FileOutputStream fs = new FileOutputStream("foo.ser");
+            ObjectOutputStream os = new ObjectOutputStream(fs);
+            os.writeObject(myBox);
+            os.close();
+        } catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
+}
